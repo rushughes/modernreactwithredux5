5740aae57e6e46577f87a00a14dcb0aad9c4171a
Rus Hughes <russell.hughes@gmail.com>
2017-10-27 15:48:12 +0700

    section 5 lecture 57

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = 'ed9c6a5d8e5eda4b68fbd43574f60dc3';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request,
+  };
+}
